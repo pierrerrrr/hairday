abcc5cee744b54dd78d5acd27572ad5fdd50be8c
pierrerrrr <pierrestorniolo@gmail.com>
2025-07-24 18:37:36 -0300

    feat: add client name input handling to form submission

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -2,6 +2,7 @@ import dayjs from 'dayjs';
 
 const form = document.querySelector('form');
 const selectedDate = document.getElementById("date")
+const clientName = document.getElementById('client');
 
 const setActualDate = dayjs(new Date()).format("YYYY-MM-DD");
 
@@ -11,4 +12,34 @@ selectedDate.min = setActualDate;
 
 form.onsubmit = (event) => {
     event.preventDefault();
+
+    try {
+        const name = clientName.value.trim();
+        if (!name) {
+            return alert("Por favor, preencha o nome do cliente.");
+        }
+
+        const hourSelected = document.querySelector(".hour-selected");
+        if (!hourSelected) {
+            return alert("Por favor, selecione um horário.");
+        }
+
+        // aqui recupera somente a hora
+        const [hour] = hourSelected.innerText.split(":");
+
+        // insere a data e hora
+        const when = dayjs(selectedDate.value).add(hour, "hour");
+
+        const id = new Date().getTime();
+
+        console.log({
+            id,
+            name,
+            when,
+        })
+
+    } catch (error) {
+        alert("Erro ao enviar o formulário. Tente novamente mais tarde.");
+        console.log(error)
+    }
 }
