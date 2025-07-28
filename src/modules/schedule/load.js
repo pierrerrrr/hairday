d617bc4e28ca05d639cfc4b526fc0cbef88305cb
pierrerrrr <pierrestorniolo@gmail.com>
2025-07-28 08:25:38 -0300

    feat: add schedule fetching functionality and update schedules handling

--- src/modules/schedule/load.js
+++ src/modules/schedule/load.js
@@ -1,9 +1,14 @@
 import { hoursLoad } from "../form/hours-load.js";
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 
 const selectedDate = document.getElementById("date")
-export function schedulesDay() {
+export async function schedulesDay() {
     // obtem somente o valor do selectedDate
     const date = selectedDate.value
     // renderiza as horas disponíveis
+
+    const dailySchedules = await scheduleFetchByDay({ date });
+    console.log(dailySchedules);
+
     hoursLoad({ date });
 }
